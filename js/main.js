7baedc870a1a5a48e017f6dfc9e2de5f4f827c3c
louisvuitsong <77098932+louisvuitsong@users.noreply.github.com>
2021-08-02 17:29:11 +0900

    1차 테스트 삭제

--- js/main.js
+++
@@ -1,8 +0,0 @@
-let menuBtn = document.querySelector('.menu_trigger');
-let menuNav = document.querySelector('.menu_nav');
-
-menuBtn.addEventListener('click', function(){
-    this.classList.toggle('active');
-    menuNav.classList.toggle('active');
-})
-
