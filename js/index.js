d75593fe29fda43281bef0f18680f75f8804c357
louisvuitsong <77098932+louisvuitsong@users.noreply.github.com>
2021-08-10 11:21:21 +0900

    20210810 1st

---
+++ js/index.js
@@ -0,0 +1,80 @@
+//배너 > 웰컴킷 구성하기
+const configTitle  = document.querySelectorAll('.config .config_list > h4');
+const configList = document.querySelectorAll('.config_nav');
+
+configTitle.forEach(function(e, key){
+    e.addEventListener('click', function(){
+        console.log('click');
+        configList[key].classList.toggle('active');
+    });
+});
+
+
+
+
+
+
+
+
+
+//배너
+// const categoryBanner = document.querySelector('.category');
+// const config = document.querySelector('.welcome_product');
+
+// window.addEventListener('scroll', function(){
+//     let configLocate = (config.offsetTop - 500);
+
+//     if(this.scrollY >= configLocate){
+//         categoryBanner.classList.add('active');
+//     } else {
+//         categoryBanner.classList.remove('active');
+//     }
+// });
+
+//모바일 메뉴버튼
+let menuBtn = document.querySelector('.menu_trigger');
+let menuNav = document.querySelector('.menu_nav');
+
+menuBtn.addEventListener('click', function(){
+    this.classList.toggle('active');
+    menuNav.classList.toggle('active');
+});
+
+
+
+
+
+
+//포트폴리오슬라이드
+const swiperPortfolio = new Swiper('.welcome_portfolio', {
+    // Optional parameters
+    spaceBetween: 50,
+    // loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+     // Responsive breakpoints
+    breakpoints: {
+       
+        // when window width is >= 480px
+        320: {
+        slidesPerView: 1.1,
+        spaceBetween: 10
+        },
+        // when window width is >= 640px
+        768: {
+        slidesPerView: 3.3,
+        spaceBetween: 40
+        }
+    },
+
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
+
---
+++ js/order.js
@@ -0,0 +1,103 @@
+//제품별 구성하기 슬라이드
+const swiperProduct = new Swiper('.welcome_product', {
+    // Optional parameters
+    spaceBetween: 50,
+    // loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+     // Responsive breakpoints
+    breakpoints: {
+       
+        // when window 'width is >= 480px
+        320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+        },
+        // when window width is >= 640px
+        768: {
+        slidesPerView: 3.3,
+        spaceBetween: 40
+        }
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        // renderCoustom(wiper, current, total)
+      },
+    // renderBullet: function (index, className) {
+    //     return '<span class="' + className + '">' + (index + 1) + "</span>";
+    //   },
+});
+
+
+//아이템 디테일 박스
+let itemDetail = document.querySelectorAll('.item_more_btn');
+const lightBox = document.querySelector('.detail_lightbox');
+
+function moreBtnClick() {
+    itemDetail.forEach(function(e){
+        e.addEventListener('click', function(){
+            lightBox.classList.toggle('active');
+        });
+    });
+}
+lightBox.addEventListener('click',function(){
+    this.classList.remove('active');
+});
+
+
+// 아이템 선택
+let itemCard = document.querySelectorAll('.item_card');
+itemCard.forEach(function(e){
+    e.addEventListener('click', function(e){
+
+        if(e.target.parentNode.classList.contains('item_more_btn')) {
+            moreBtnClick();
+        } else {
+            this.classList.toggle('active');
+        }
+    })
+});
+
+
+//장바구니 늘이거나 줄이기
+const cart = document.querySelector('.cart');
+
+cart.addEventListener('click', function(){
+    this.classList.toggle('active');
+});
+
+
+//웰컴킷 구성하기 버튼
+const configNavBtn = document.querySelectorAll('.config_nav a');
+
+configNavBtn.forEach(function(e, key){
+    e.addEventListener('click', function(){
+
+        this.classList.toggle('active');
+    });
+});
+
+const recommand = document.querySelector('.recommand');
+const choice = document.querySelector('.choice');
+
+configNavBtn[0].addEventListener('click', function(){
+    console.log(this);
+    choice.classList.remove('active');
+
+    recommand.classList.add('active');
+
+});
+
+configNavBtn[2].addEventListener('click', function(){
+
+    recommand.classList.remove('active');
+
+    choice.classList.add('active');
+
+});
